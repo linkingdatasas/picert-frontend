e26c6ffa6ef4e255664d71863ebfbc955c92ccc6
ccastroiragorri <ccastroiragorri@gmail.com>
2019-09-25 16:15:46 -0500

    initial angular app

---
+++ src/app/Services/user.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class User {
+    constructor() {}
+
+    public
+
+    public autenticate(username: string, pass: string) {
+        return
+    }
+
+}
